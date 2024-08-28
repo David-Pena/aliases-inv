ab9672fb21932b7831f14cf81540e079f1eaf8c0
David-Pena <dev.davidpe@gmail.com>
2024-08-27 22:04:39 -0700

    chore: apply format script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entryPoints: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  entryPoints: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
-  outDir: "dist",
-  clean: true
-})
+  outDir: 'dist',
+  clean: true,
+});
